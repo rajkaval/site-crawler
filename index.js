86b388baf8a34a30872e1e580b87f23fc88d9adb
Raja Kavalam <raja.kavalam@digitas.com>
2021-12-03 17:58:20 +0000

    Disabling autoScrolling

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ var pageUrls = require('./pages.json');
 console.log("Starting to crawl the page urls mentioned in pages.json");
 (async () => {
   const revisionInfo = await browserFetcher.download(848005);
-
+  console.log("Chromium browser is available now");
   const browser = await puppeteer.launch({
-        executablePath: revisionInfo.executablePath
+        executablePath: revisionInfo.executablePath,
+        headless: true
       });
   //const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -17,11 +18,11 @@ console.log("Starting to crawl the page urls mentioned in pages.json");
   for(var i=0;i<pageUrls.length;i++){
     let url = pageUrls[i];
     await page.goto(url);
-    await autoScroll(page);
+    //await autoScroll(page);
     await page.screenshot({ path: `page${i+1}.png` });
     console.log(`Page ${i+1} - ${url} - finished`);
   }
-  console.log("Finished to crawling pages");  
+  console.log("Finished crawling pages");  
   await browser.close();
 })();
 
